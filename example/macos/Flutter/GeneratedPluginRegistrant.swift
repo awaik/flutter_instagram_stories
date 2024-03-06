95c1cbca9a13285cb847baa9a0a434309f006c1e
awaik <awaiking44@gmail.com>
2024-03-06 15:47:14 +0100

    update dependencies

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import cloud_firestore
 import firebase_core
 import path_provider_foundation
 import sqflite
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  cloud_firestore
+  firebase_core
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
